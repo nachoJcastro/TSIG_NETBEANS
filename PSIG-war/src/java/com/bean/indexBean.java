d7a83daf525a813319ecd91cd0f323775a5bc3a3
vancas <caseres.vanessa@gmail.com>
2015-05-09 13:28:05 -0300

    se realizo correctamente la creacion de un administrador en la base de datos

---
+++ PSIG-war/src/java/com/bean/indexBean.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bean;
+
+import com.logica.AdministradorL;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author vane
+ */
+@ManagedBean
+@RequestScoped
+public class indexBean {
+    
+    private String login;
+    private String clave;
+    private String nombre;
+    private String apellido;
+    private int cedula;
+    private int telefono;
+    private String email;
+    private boolean rol;
+    
+    
+    @EJB
+    private AdministradorL AdmL;
+    
+    public indexBean(){
+    }
+    
+    public boolean crearAdministrador(){
+        
+        AdmL.crearAdministrador(login, clave, nombre, apellido, cedula, telefono, email, rol);
+        return true;
+    }
+    
+     public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(Integer cedula) {
+        this.cedula = cedula;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getRol() {
+        return rol;
+    }
+
+    public void setRol(Boolean rol) {
+        this.rol = rol;
+    }
+}
