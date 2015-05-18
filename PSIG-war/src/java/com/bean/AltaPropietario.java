f7e94d17547d32d370cfb23612c0ee226d3724a6
nacheen <nacheen@nacheen-PC>
2015-05-18 17:40:08 -0300

    se comiteo el PSIG-war con AltaPropietario

---
+++ PSIG-war/src/java/com/bean/AltaPropietario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bean;
+import com.logica.PropietarioL;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author nacheen
+ */
+public class AltaPropietario {
+        @EJB
+    private PropietarioL propL;
+
+    
+    public AltaPropietario() {
+    }
+}
